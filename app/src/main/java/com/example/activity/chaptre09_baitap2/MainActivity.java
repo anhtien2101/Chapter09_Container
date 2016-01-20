f5a925fa204abaf6ad1d65d0e8003f7fdd212d15
Anh Tien <anhtien210@gmail.com>
2016-01-20 13:31:13 +0700

    first commit

---
+++ app/src/main/java/com/example/activity/chaptre09_baitap2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.activity.chaptre09_baitap2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
